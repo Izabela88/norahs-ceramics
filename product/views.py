fdec4e4b737c88d1f5e750f27955c2a84bba5b7e
Izabela88 <izabkru88@gmail.com>
2022-05-27 13:53:58 +0100

    Add filtering for product list

--- product/views.py
+++ product/views.py
@@ -8,6 +8,14 @@ class ProductListView(ListView):
    context_object_name = "product_list"
    template_name = "product/product-list.html"
 
+   def get_queryset(self):
+      product_query = Product.objects.all()
+      if sub_category := self.request.GET.get("sub_category"):
+         product_query = product_query.filter(sub_category__name=sub_category)
+      if category := self.request.GET.get("category"):
+         product_query = product_query.filter(sub_category__category_id=category)     
+      return product_query
+
 
 class ProductDetailView(DetailView):
    model = Product
