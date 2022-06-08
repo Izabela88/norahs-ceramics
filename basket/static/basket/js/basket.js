dcda3a73a0dd33fcb65778f9fcb207d064c9e240
Izabela88 <izabkru88@gmail.com>
2022-06-08 11:45:06 +0100

    Correct basket style

--- basket/static/basket/js/basket.js
+++ basket/static/basket/js/basket.js
@@ -1,1 +1,6 @@
-
+// {% else %}
+// <div class="empty-basket">
+//     <p>your basket is empty</p>
+//     <a class="button button-dark" href="{% url 'product_list' %}">back to shopping</a>
+// </div>
+// {% endif %}
