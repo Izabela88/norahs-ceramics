9583ef06b216584aee676e5671bed46f2abce7d5
Izabela88 <izabkru88@gmail.com>
2023-02-19 15:03:40 +0000

    Update settings

--- norahs_ceramics/settings.py
+++ norahs_ceramics/settings.py
@@ -186,13 +186,13 @@ USE_TZ = True
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
 STATIC_URL = "/static/"
-# STATICFILES_STORAGE = (
-#     "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
-# )
+STATICFILES_STORAGE = (
+    "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
+)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
